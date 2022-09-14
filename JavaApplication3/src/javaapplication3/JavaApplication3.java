c54b433ccf0bf9dc0b12275cb3801b83b54ff649
Agustin Pereyra <agus.pereyra835@gmail.com>
2022-09-14 20:54:53 -0300

    Prueba

---
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+    import java.util.Scanner;
+/**
+ *
+ * @author Agus
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      /*  int n1 = 4;
+        int n2 = 5;
+        int n3 = n1 + n2;
+        int n4 = n3 / (n1-2);    
+      
+      
+        int e1 = 30;
+        String nombre = "Agustin Pereyra";
+        
+        System.out.println("Mi nombre es " + nombre + " y tengo " + e1);  
+      
+      
+      Scanner leer = new Scanner(System.in);
+      int numero = leer.nextInt();
+            System.out.println(numero);    */
+      
+    }
+    
+}
---
+++ NombreGuiaEj1/src/nombreguiaej1/NombreGuiaEj1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nombreguiaej1;
+
+/**
+ *
+ * @author Agus
+ */
+public class NombreGuiaEj1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte num = 10;
+        float decimal =10;
+        char caracter = 'a';
+        boolean logico;
+        String cadena = "Agustin Pereyra";
+        double decibien = 13.2;
+    }
+    
+}
+
